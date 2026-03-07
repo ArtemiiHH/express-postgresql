d632cdc841e2f1da296eb385b64e05fa165d8435
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-07 19:11:54 +0200

    Setup app, index view

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const userRouter = require("./routes/userRoutes");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", userRouter);
+
+// Server
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log(`Express app listening on localhost:${PORT}`);
+});
