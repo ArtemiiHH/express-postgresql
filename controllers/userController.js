8500221de0bed4b214f48798a6a49f0ca3b8f9df
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-08 18:43:55 +0200

    Modify controller with async funcs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,23 @@
-exports.getUser = (req, res) => {
-  console.log("usernames will be logged here - wip");
-  res.send("username");
-};
+const db = require("../db/queries");
 
-exports.displayForm = (req, res) => {
-  res.render("form", { title: "Create username" });
-};
+async function getUsernames(req, res) {
+  const usernames = await db.getAllUsernames();
+  console.log("Usernames: ", usernames);
+  res.send("Usernames: " + usernames.map((user) => user.username).join(", "));
+}
 
-exports.handleSubmit = (req, res) => {
-  console.log("username to be saved: ", req.body.username);
+async function createUsernameGet(req, res) {
+  res.render("form");
+}
+
+async function createUsernamePost(req, res) {
+  const { username } = req.body;
+  await db.insertUsername(username);
   res.redirect("/");
+}
+
+module.exports = {
+  getUsernames,
+  createUsernameGet,
+  createUsernamePost,
 };
--- db/queries.js
+++ db/queries.js
@@ -1,10 +1,13 @@
+// Database access module
 const pool = require("./pool");
 
+// Get all rows
 async function getAllUsernames() {
   const { rows } = await pool.query("SELECT * FROM usernames");
   return rows;
 }
 
+// Insert new username into db
 async function insertUsername(username) {
   await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
 }
