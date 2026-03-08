12a2e5b4d88111770981aa48f4cdaed2d669a50b
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-08 19:00:40 +0200

    Add populatedb logic

---
+++ db/populatedb.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+require("dotenv").config();
+const { Client } = require("pg");
+
+const SQL = `
+  CREATE TABLE IF NOT EXISTS usernames (
+    id INTEGER PRIMARY KEY GENERATED AWLAYS AS IDENTITY,
+    username VARCHAR ( 255 )
+  );
+
+  INSERT INTO usernames (username)
+  VALUES 
+    ('Bryan'),
+    ('Odin'),
+    ('Damon');
+`;
+
+async function main() {
+  console.log("...seeing");
+  const client = new Client({
+    connectionString: process.env.DB_URL,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
